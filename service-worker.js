6fe6e1556f86bc82c8aebdd34b087dee3c89b0fa
Rhavyx <everluca@outlook.com>
2021-09-19 20:40:40 -0300

    :sparkles: disable pane

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c2488b63f6c5efcf5076026335c2afc.js"
+  "/precache-manifest.3df62d1b5b18c982be161595e4b32e36.js"
 );
 
 self.addEventListener('message', (event) => {
