300b299879d3632b79a066e2d0ae4e88b7437f4f
Rhavyx <everluca@outlook.com>
2021-09-19 20:28:30 -0300

    :bug: small changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e011396df0df09b6cc83cce0d7021a80.js"
+  "/precache-manifest.8c2488b63f6c5efcf5076026335c2afc.js"
 );
 
 self.addEventListener('message', (event) => {
