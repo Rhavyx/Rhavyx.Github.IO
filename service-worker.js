007a7a8a775354d55cc1025cdfb31a59849c14a0
Rhavyx <everluca@outlook.com>
2021-09-21 09:41:45 -0300

    :bug: layout fix again

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.479945adb753514bd54e2dd65180451b.js"
+  "/precache-manifest.1a5044d1fecba3639e12f83ad65e29f0.js"
 );
 
 self.addEventListener('message', (event) => {
